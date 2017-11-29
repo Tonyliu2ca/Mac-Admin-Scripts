5c461548e167323f21d20f6870608b20d33e6c43
ApplePi(苹果派) T.S.L <tonyliu2ca@yahoo.com>
2017-11-29 10:47:14 -0700

    v1.1
    add comments

--- fix_rootenablebug_osx13.sh
+++ fix_rootenablebug_osx13.sh
@@ -6,4 +6,11 @@
 for keys in authentication_authority KerberosKeys ShadowHashData; do
    sudo defaults delete /var/db/dslocal/nodes/Default/users/root.plist $keys &>/dev/null
 done
+
+# Comments for the last command below.
+# Even root is re-enabled in Directory Utility or other GUI way, the "shell" stay the false, to set it back to system default:
+# sudo defaults write /var/db/dslocal/nodes/Default/users/root.plist shell "/bin/sh"
+# or
+# sudo dscl . append /users/root UserShell "/bin/sh"
+#
 sudo defaults write /var/db/dslocal/nodes/Default/users/root.plist shell "/usr/bin/false"
