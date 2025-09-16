1ffdb203320773961039a25154b357f91de7a20c
ApplePi(苹果派) T.S.L <tonyliu2ca@yahoo.com>
2025-09-16 17:06:55 -0600

    a wrapper to quick make Ventura installer
    Make sure you use Initliaize.sh to initialize your USB drive first.

---
+++ mk3MacInstallerMultiDisks/Ventura.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+scriptpath=$(dirname "$0")
+
+cd "$scriptpath" || return
+app=""
+if [ -z "$1" ]; then
+    app="./Install macOS Ventura.app"
+else
+    if [ -x "$1/Contents/Resources/createinstallmedia" ]; then
+        app="$1"
+    fi
+fi
+[ -n "$app" ] && yes | sudo ./mk3InstallerMultiDisks -2 "$app" -d disk+
